ff8d929ef3f3c7cff5abbdf4d5eadc61264a95aa
Developer <developer@example.com>
2026-07-27 22:17:45 +0900

    Remove distracting animations to restore 100% pure focus screen (v78.1.0)

--- js/game.js
+++ js/game.js
@@ -15,8 +15,6 @@ class Game {
         this.particles = [];
         this.shockwaves = [];
         this.comboPopups = [];
-        this.comboRipples = [];
-        this.scoreSparks = [];
         this.gameSpeed = 1.0;
 
         this.ringPulse = 0;
@@ -474,20 +472,10 @@ class Game {
                 hit = true;
                 const isPerfect = diff <= 8;
 
-                if (isPerfect) {
-                    // 🌟 3. PERFECTスコア吸い込み流星エフェクト発生！
-                    this.scoreSparks.push(new ScoreAbsorbSpark(touchX, touchY, 65, 32, '#ffcc00'));
-                }
-
                 enemy.onHit(this, touchX, touchY, isPerfect);
 
                 if (enemy.hp <= 0) {
                     this.enemies.splice(i, 1);
-                }
-
-                // 💫 1. コンボ波紋リング (10, 20, 30... コンボ到達時)
-                if (this.combo > 0 && this.combo % 10 === 0) {
-                    this.comboRipples.push(new ComboRipple(centerX, centerY, this.player.targetRadius, '#ffea00'));
                 }
                 break;
             }
@@ -645,22 +633,6 @@ class Game {
             p.update();
             p.draw(this.ctx);
             if (p.alpha <= 0) this.particles.splice(i, 1);
-        }
-
-        // 💫 1. コンボ波紋リング (Combo Ripple Pulse) 更新・描画
-        for (let i = this.comboRipples.length - 1; i >= 0; i--) {
-            const cr = this.comboRipples[i];
-            cr.update();
-            cr.draw(this.ctx);
-            if (cr.alpha <= 0) this.comboRipples.splice(i, 1);
-        }
-
-        // 🌟 3. PERFECTスコア吸い込み流星 (Score Spark Absorber) 更新・描画
-        for (let i = this.scoreSparks.length - 1; i >= 0; i--) {
-            const ss = this.scoreSparks[i];
-            ss.update();
-            ss.draw(this.ctx);
-            if (ss.alpha <= 0) this.scoreSparks.splice(i, 1);
         }
 
         requestAnimationFrame(() => this.gameLoop());
